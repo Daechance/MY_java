159f2a869c7c586af7de639e163029d13f12dcaf
Daechance <91683717+Daechance@users.noreply.github.com>
2022-03-30 01:22:21 +0900

    plus sub_class

---
+++ java_ppt4_sub_class/src/main/java_class.java
@@ -0,0 +1,16 @@
+package main;
+
+public class java_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java_sub_class plus = new java_sub_class();
+		
+		plus.num1 = 10;
+		plus.num2 = 13;
+		int sum = plus.cal();
+		System.out.printf("%d",sum);
+		
+	}
+
+}
---
+++ java_ppt4_sub_class/src/main/java_sub_class.java
@@ -0,0 +1,16 @@
+package main;
+
+public class java_sub_class {
+	int num1;
+	int num2;
+	protected int cal()
+	{
+		return num1 + num2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
